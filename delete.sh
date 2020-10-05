2b05a47664e915fcfb0988fb889b789696e0b49a
Andy Prowse <aprowse@highvail.com>
2020-10-05 15:46:42 -0400

    init config

--- delete.sh
+++
@@ -1,7 +0,0 @@
-oc --context hub delete -f https://raw.githubusercontent.com/zombiefish/acm-app-lifecycle-blog/master/acm-manifests/reversewords-stage/03_application-dev.yaml
-
-oc --context hub delete -f https://raw.githubusercontent.com/zombiefish/acm-app-lifecycle-blog/master/acm-manifests/reversewords-stage/02_subscription-dev.yaml
-
-oc --context hub delete -f https://raw.githubusercontent.com/zombiefish/acm-app-lifecycle-blog/master/acm-manifests/reversewords-stage/01_placement_rule.yaml
-
-oc --context hub delete -f https://raw.githubusercontent.com/zombiefish/acm-app-lifecycle-blog/master/acm-manifests/reversewords-stage/00_namespace.yaml
